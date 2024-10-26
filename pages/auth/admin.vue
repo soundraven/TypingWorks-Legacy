94effee33d4a00c2d7802629d5a36e270302f939
soundraven <kin9709@gmail.com>
2024-10-26 19:51:55 +0900

    admin 페이지 테이블 구조 변경

--- pages/auth/admin.vue
+++ pages/auth/admin.vue
@@ -3,21 +3,63 @@
     <div :class="$style.container">
       <ClientOnly>
         <el-table :data="requestList" :highlight-current-row="true">
-          <el-table-column prop="id" label="ID"></el-table-column>
-          <el-table-column prop="requester" label="신청자"></el-table-column>
-          <el-table-column prop="langauge" label="언어"></el-table-column>
+          <el-table-column type="expand">
+            <template #default="props">
+              <div style="margin: 12px">
+                <p style="margin-top: 0; margin-bottom: 6px">
+                  언어: {{ props.row.language }}
+                </p>
+                <p style="margin-top: 0; margin-bottom: 6px">
+                  문장 유형: {{ props.row.type }}
+                </p>
+                <p style="margin-top: 0; margin-bottom: 6px">
+                  출처: {{ props.row.source }}
+                </p>
+                <p style="margin-top: 0; margin-bottom: 6px">
+                  문장: {{ props.row.sentence }}
+                </p>
+                <p
+                  v-if="props.row.explanation !== null"
+                  style="margin-top: 0; margin-bottom: 6px"
+                >
+                  설명: {{ props.row.explanation }}
+                </p>
+                <p
+                  v-if="props.row.other_type === 1"
+                  style="margin-top: 0; margin-bottom: 6px"
+                >
+                  신청한 유형: {{ props.row.comment }}
+                </p>
+              </div>
+            </template>
+          </el-table-column>
           <el-table-column
-            prop="sentenseType"
-            label="문장 유형"
+            prop="id"
+            label="ID"
+            align="center"
           ></el-table-column>
           <el-table-column
-            prop="otherSentenceType"
-            label="새 유형 신청"
+            prop="requester"
+            label="신청자"
+            align="center"
           ></el-table-column>
-          <el-table-column prop="comment" label="신청한 유형"></el-table-column>
-          <el-table-column prop="source" label="문장 출처"></el-table-column>
-          <el-table-column prop="sentence" label="문장 내용"></el-table-column>
-          <el-table-column prop="explanation" label="설명"></el-table-column>
+          <el-table-column prop="other_type" label="새 유형" align="center">
+            <template #default="props">
+              <el-switch
+                v-model="props.row.other_type"
+                inline-prompt
+                active-icon="Check"
+                inactive-icon="Close"
+                disabled
+              ></el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column label="확인" align="center">
+            <div>
+              <el-button type="success" icon="Check" circle />
+              <el-button type="danger" icon="Delete" circle />
+            </div>
+          </el-table-column>
         </el-table>
       </ClientOnly>
     </div>
@@ -44,10 +86,11 @@ onMounted(async () => {
   min-height: 100dvh;
   display: flex;
   justify-content: center;
+  padding: 12px;
 
   > .container {
-    width: 1000px;
-    min-height: 600px;
+    width: 650px;
+    min-height: 100dvh;
 
     display: flex;
     justify-content: center;
